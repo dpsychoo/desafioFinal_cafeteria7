8f2c9418043f8d60a1ab30b831934e6b6e1efeb1
David Castro <phoenix6ccxr@gmail.com>
2025-02-03 05:35:49 -0300

    desafio7Final_coffr2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const app = express();
 const cafes = require("./cafes.json");
 
-app.listen(3000, console.log("SERVER ON en puerto 3000"));
+app.listen(3000, console.log("SERVER ON en puerto 3000")); //o PORT 4000
 
 app.use(express.json());
 
